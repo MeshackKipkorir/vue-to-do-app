dc1c6e549bd5205d430ed88432d4925bab840610
MeshackKipkorir <meshackkipkorir105@gmail.com>
2021-01-22 14:29:59 +0300

    displayed todo tasks

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Todo_list v-bind:todos="todos" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import Todo_list from './components/Todo_list';
+
+export default{
+    name:'app',
+    components:{
+      Todo_list
+    },
+    data(){
+      return{
+      todos:[
+        {
+          id:1,
+          title:'Go WorkOut',
+          completed:false
+        },
+        {
+          id:2,
+          title:'Learn Vue Js',
+          completed:true
+        },
+        {
+          id:3,
+          title:'Shopping',
+          completed:true
+        },
+        {
+          id:4,
+          title:'Go play football',
+          completed:false
+        },
+        {
+          id:5,
+          title:'Do dishes',
+          completed:false
+        }
+      ],
+      }
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ todo-app/src/components/Todo_list.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>To do list</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for="todo in todos">
+        <Todo v-bind:todo="todo"/>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  import Todo from './Todo'
+
+  export default{
+    name:'Todo_list',
+    components: {
+      Todo
+    },
+    props:[
+    "todos"
+    ]
+  }
+</script>
