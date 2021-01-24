88a81a0454761ac37450c8ad2a7c78cabe499678
MeshackKipkorir <meshackkipkorir105@gmail.com>
2021-01-24 06:57:30 +0300

    implemented add to do

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,49 +1,58 @@
 <template>
   <div id="app">
     <Todo_list v-bind:todos="todos" />
+
+    <add_to_do_list v-on:add-todo="addToDo" />
   </div>
 </template>
 
 <script>
 
 import Todo_list from './components/Todo_list';
+import add_to_do_list from './components/add_to_do_list';
 
 export default{
     name:'app',
     components:{
-      Todo_list
+      Todo_list,
+      add_to_do_list,
     },
     data(){
       return{
-      todos:[
-        {
-          id:1,
-          title:'Go WorkOut',
-          completed:false
-        },
-        {
-          id:2,
-          title:'Learn Vue Js',
-          completed:true
-        },
-        {
-          id:3,
-          title:'Shopping',
-          completed:true
-        },
-        {
-          id:4,
-          title:'Go play football',
-          completed:false
-        },
-        {
-          id:5,
-          title:'Do dishes',
-          completed:false
-        }
-      ],
+        todos:[
+          {
+            id:1,
+            title:'Go WorkOut',
+            completed:false
+          },
+          {
+            id:2,
+            title:'Learn Vue Js',
+            completed:true
+          },
+          {
+            id:3,
+            title:'Shopping',
+            completed:true
+          },
+          {
+            id:4,
+            title:'Go play football',
+            completed:false
+          },
+          {
+            id:5,
+            title:'Do dishes',
+            completed:false
+          }
+        ],
       }
     },
+      methods: {
+        addTodo(newTodoObj) {
+          this.todos = [...this.todos, newTodoObj];
+        }
+    }
   }
 </script>
 
---
+++ todo-app/src/components/add_to_do_list.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+     <form @submit="addToDo">
+        <input type="text" v-model="title" name="title">
+        <button type="submit">Add</button>
+     </form>
+  </div>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+
+  export default{
+    name:'add_to_do_list',
+    data(){
+      return{
+        title:''
+      }
+    },
+    methods:{
+      addToDo(e){
+        e.preventDefault();
+
+        const newToDoObj = {
+          id: uuidv4,
+          title:this.title,
+          completed:false
+        }
+
+        this.$emit('add-todo', newToDoObj);
+        this.title = '';
+      }
+    }
+  }
+
+</script>
